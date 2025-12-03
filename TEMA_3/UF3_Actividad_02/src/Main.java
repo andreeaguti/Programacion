25bbd6c4785532ad67e72d4a1fbc6e73e89b546e
COMPUS029\daw1 <agutierrezp18@educantabria.es>
2025-12-03 09:48:54 +0100

    haciendo el main

--- TEMA_3/UF3_Actividad_02/src/Main.java
+++ TEMA_3/UF3_Actividad_02/src/Main.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc;
+        Scanner sc = new Scanner(System.in);
 
         String opcion = "";
         do {
-            sc = new Scanner(System.in);
             imprimirMenuOpciones();
             opcion = sc.nextLine();
 
@@ -22,12 +21,40 @@ public class Main {
                     String codigo = sc.nextLine();
                     System.out.print("Ingrese el titulo de la pelicula: ");
                     String titulo = sc.nextLine();
-                    System.out.print("Ingrese el genero de la pelicula: ");
-                    Genero genero;
+
+                    Genero[] generosDisponibles = Genero.values();
+                    Genero generoSeleccionado = null;
+                    int opcionGeneros = -1;
+                    do {
+                        System.out.print("Elija el genero de la pelicula: \n");
+                        for (int i = 0; i < generosDisponibles.length; i++) {
+                        System.out.println("  " + (i + 1) + ". " + generosDisponibles[i]);
+                        }
+                        System.out.print("Ingrese el numero de opcion: ");
+                        if (sc.hasNextInt()) {
+                            opcionGeneros = sc.nextInt();
+                        }else {
+                            opcionGeneros = -1;
+                            sc.nextLine();
+                        }
+                        if (opcionGeneros >= 1 && opcionGeneros <= generosDisponibles.length) {
+                            // Si es válido, asigna el enum
+                            generoSeleccionado = generosDisponibles[opcionGeneros - 1];
+                        } else {
+                            System.err.println("Opción no válida. Por favor, ingrese un número entre 1 y " + generosDisponibles.length + ".");
+                        }
+                    } while (generoSeleccionado == null);
+                    Pelicula pelicula = new Pelicula(codigo, titulo, generoSeleccionado);
                     System.out.println("Pelicula con codigo: " + codigo + " creada con exito.");
-                    /** Pelicula pelicula = new Pelicula (codigo, titulo, genero);
-                    System.out.println(pelicula.mostrarInfoPelicula());
-                    VideoDaw.agregarPelicula((pelicula)); **/
+                    System.out.println(pelicula.mostrarInfoPelicula()); //muestra la información de la película
+                    break;
+                case "3":
+                    System.out.print("Introduzca su DNI: ");
+                    String dni = sc.nextLine();
+                    System.out.print("Introduzca su nombre: ");
+                    String nombre = sc.nextLine();
+                    String codigoValido = MiUtils.comprobarPatronRepetidamente("[A-Z]{1}-[0-9]{4}", "Introduzca código de cliente");
+                    String direccionCliente = MiUtils.comprobarPatronRepetidamente(".+", "Introduzca su dirección: ");
 
 
 
@@ -39,12 +66,8 @@ public class Main {
             System.out.println("Escoga la opcion que desee");
             System.out.println("1. Crear y registrar VideoClub en la franquicia.");
             System.out.println("2. Registrar película en videoclub.");
-            System.out.println("3. Titular");
-            System.out.println("4. Saldo");
-            System.out.println("5. Ingreso");
-            System.out.println("6. Retirada");
-            System.out.println("7. Movimientos");
-            System.out.println("8. Salir");
+            System.out.println("3. Crear y registrar cliente en videoclub");
     }
+
 
 }
