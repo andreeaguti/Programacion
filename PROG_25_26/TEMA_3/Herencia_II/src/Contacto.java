382e7d305c349057f86e055a55df5a4c61913556
COMPUS029\daw1 <agutierrezp18@educantabria.es>
2025-12-17 10:05:21 +0100

    empezando herencia II

---
+++ PROG_25_26/TEMA_3/Herencia_II/src/Contacto.java
@@ -0,0 +1,19 @@
+public class Contacto {
+
+    private String nombre;
+    private String telefono;
+
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
