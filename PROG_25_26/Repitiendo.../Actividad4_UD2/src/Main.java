e0472600c91f0d49315f9deaf6d5218d43db709d
Andrea Gutierrez <andreeaguti@icloud.com>
2025-12-26 13:33:35 +0100

    continuando

--- PROG_25_26/Repitiendo.../Actividad4_UD2/src/Main.java
+++ PROG_25_26/Repitiendo.../Actividad4_UD2/src/Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Ejercicio 01
+        System.out.println("Ejercicio 01");
+        //Crea un programa que pida diez números reales por teclado, los almacene en un array,
+        // y luego muestre todos sus valores.
+        double[] numerosReales = new double[10];
+        //inicia un bucle for para recorrer el array
+        for (int i = 0; i < numerosReales.length; i++) {
+            System.out.println("Ingrese diez numeros reales:");
+            numerosReales[i] = sc.nextDouble(); //asigna cada número a la posición i del array, es decir, el primer número lo guarda en la posición 0
         }
+        System.out.println(Arrays.toString(numerosReales));
+        // Arrays.toString es un metodo que convierte el contenido de un array en texto legible
+
+        //Ejercicio 02
+        System.out.println("Ejercicio 02");
+        sc = new Scanner(System.in);
+
+        double[] numerosReales1 = new double[10];
+        double suma = 0.0; //creo una variable para sumar
+
+        for (int i = 0; i < numerosReales1.length; i++) {
+            System.out.println("Introduce el numero real " + (i+1) + ": ");
+            numerosReales1[i] = sc.nextDouble(); //coge el número que el usuario escribe y lo guarda en la posición del array
+            suma += numerosReales1[i]; //es lo mismo que suma = suma + numerosReales[i]
+        }
+
+        System.out.println("La suma es: " + suma);
+
+        //Ejercicio 03
+        System.out.println("Ejercicio 03");
+        sc = new Scanner(System.in);
+
+        double[] numerosReales2 = new double[10];
+
+        for (int i = 0; i < numerosReales1.length; i++) {
+            System.out.println("Introduce el numero real " + (i+1) + ": ");
+            numerosReales2[i] = sc.nextDouble(); //coge el número que el usuario escribe y lo guarda en la posición del array
+        }
+        System.out.println(Arrays.toString(numerosReales2));
+        
+
+
+
+
+
+
+
+
+
+
+
     }
 }
