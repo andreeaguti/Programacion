e8b4e8017ef2ad174a2bf99debe5aa1eb503739d
COMPUS029\daw1 <agutierrezp18@educantabria.es>
2026-01-23 14:27:57 +0100

    nadaaaaaaaaaaaaaaaaa

--- PROG_25_26/TEMA_5/Actividad01_UD5/src/Main.java
+++ PROG_25_26/TEMA_5/Actividad01_UD5/src/Main.java
@@ -95,7 +95,7 @@ funciones, etc. Maneja las posibles excepciones. */
         System.out.println("Error de lógica: " + e.getMessage());
     }
 
-        /*Implementa una clase Gato con los atributos nombre y edad, un constructor con parámetros,
+        /*EJERCICIO 5: Implementa una clase Gato con los atributos nombre y edad, un constructor con parámetros,
 los getters y setters, además de un metodo imprimir() para mostrar los datos de un gato. El
 nombre de un gato debe tener al menos 3 caracteres y la edad no puede ser negativa. Por ello,
 tanto en el constructor como en los setters, deberás comprobar que los valores sean válidos y
